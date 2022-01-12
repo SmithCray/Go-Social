6b9dc5164cc605e6baf1f8d711b51357256ac63b
Cray Smith <cmsmith004@gmail.com>
2022-01-11 20:22:04 -0700

    Structure build, added files for models/ index/ server.js

---
+++ models/Thought.js
@@ -0,0 +1,46 @@
+const { Schema, model } = require("mongoose");
+const reactionSchema = require("./Reaction.js");
+
+const userSchema = new Schema(
+  {
+    userName: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      // add email verify here
+    },
+    thoughts: [
+      {
+        type: Schema.Type.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Type.ObjectId,
+        ref: "User",
+      },
+    ],
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
+const User = model("User", userSchema);
+
+module.exports = User;
