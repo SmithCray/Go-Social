d50811d95cfc9627e5e54265e8edbcd7b11fa8e1
Cray Smith <cmsmith004@gmail.com>
2022-01-22 21:23:07 -0700

    Added and testing opperations and routes, working array display Thoughts

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,4 +1,13 @@
 // ref user routes
 const router = require("express").Router();
+const {
+  getThoughts,
+  getThought,
+  addThought,
+  updateThought,
+} = require("../../controllers/thoughtController");
+
+router.route("/").get(getThoughts).post(addThought);
+router.route("/:id").get(getThought).put(updateThought);
 
 module.exports = router;
